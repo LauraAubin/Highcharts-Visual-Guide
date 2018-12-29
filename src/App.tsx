0c6798d77bc547e8f8af26255721e7cb9bd2379a
Laura Aubin <laura.aubin@shopify.com>
2018-12-28 19:13:50 -0500

    Create navigation sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import * as React from "react";
-import { AppProvider, Frame, TopBar, Stack } from "@shopify/polaris";
+import {
+  AppProvider,
+  Frame,
+  TopBar,
+  Stack,
+  Navigation,
+  ChoiceList
+} from "@shopify/polaris";
 import appLogo from "./images/HighchartsVisualGuide.svg";
 import Card from "./Card";
 
@@ -7,20 +14,25 @@ import "./App.scss";
 
 interface State {
   searchText: string;
+  navigationSelect: [string];
 }
 
 class App extends React.Component<{}, State> {
   constructor(state: State) {
     super(state);
-    this.state = { searchText: "" };
+    this.state = { searchText: "", navigationSelect: [""] };
   }
 
   handleSearchField = (value: string) => {
     this.setState({ searchText: value });
   };
 
+  handleNavigationSelect = (value: [string]) => {
+    this.setState({ navigationSelect: value });
+  };
+
   public render() {
-    const { searchText } = this.state;
+    const { searchText, navigationSelect } = this.state;
     const teal = "#357997";
 
     const theme = {
@@ -48,51 +60,72 @@ class App extends React.Component<{}, State> {
 
     const topBarMarkup = <TopBar searchField={searchFieldMarkup} />;
 
+    const navigationMarkup = (
+      <Navigation location="/">
+        <div className="NavigationContainer">
+          <div className="NavigatorTitle">Only show graphs of type</div>
+          <ChoiceList
+            allowMultiple
+            choices={[
+              {
+                label: "plotOptions",
+                value: "plotOptions"
+              }
+            ]}
+            selected={navigationSelect}
+            onChange={this.handleNavigationSelect}
+          />
+        </div>
+      </Navigation>
+    );
+
     return (
       <AppProvider theme={theme}>
-        <Frame topBar={topBarMarkup}>
-          <Stack>
-            <Card
-              name="Line color"
-              description="Main color of the line itself"
-              link="https://api.highcharts.com/highcharts/plotOptions.line.color"
-              implementation="{plotOptions: {line: {color: value}}"
-              textFieldPlaceHolder="Default: #7CB5EC"
-            />
-            <Card
-              name="Line width"
-              description="Pixel width of the graph line"
-              link="https://api.highcharts.com/highcharts/plotOptions.line.lineWidth"
-              implementation="{plotOptions: {line: {lineWidth: value}}"
-              textFieldPlaceHolder="Default: 2"
-            />
-            <Card
-              name="Cursor type"
-              description="Mouse icon on graph hover. i.e, pointer"
-              link="https://api.highcharts.com/highcharts/plotOptions.line.cursor"
-              implementation="{plotOptions: {line: {cursor: value}}"
-              textFieldPlaceHolder="Default: undefined"
-            />
-            <Card
-              name="Dash style"
-              popoverItems={[
-                "Solid",
-                "ShortDash",
-                "ShortDot",
-                "ShortDashDot",
-                "ShortDashDotDot",
-                "Dot",
-                "Dash",
-                "LongDash",
-                "DashDot",
-                "LongDashDot",
-                "LongDashDotDot"
-              ]}
-              link="https://api.highcharts.com/highcharts/plotOptions.line.dashStyle"
-              implementation="{plotOptions: {line: {dashStyle: value}}"
-              textFieldPlaceHolder="Default: solid"
-            />
-          </Stack>
+        <Frame topBar={topBarMarkup} navigation={navigationMarkup}>
+          <div className="CardContainer">
+            <Stack>
+              <Card
+                name="Line color"
+                description="Main color of the line itself"
+                link="https://api.highcharts.com/highcharts/plotOptions.line.color"
+                implementation="{plotOptions: {line: {color: value}}"
+                textFieldPlaceHolder="Default: #7CB5EC"
+              />
+              <Card
+                name="Line width"
+                description="Pixel width of the graph line"
+                link="https://api.highcharts.com/highcharts/plotOptions.line.lineWidth"
+                implementation="{plotOptions: {line: {lineWidth: value}}"
+                textFieldPlaceHolder="Default: 2"
+              />
+              <Card
+                name="Cursor type"
+                description="Mouse icon on graph hover. i.e, pointer"
+                link="https://api.highcharts.com/highcharts/plotOptions.line.cursor"
+                implementation="{plotOptions: {line: {cursor: value}}"
+                textFieldPlaceHolder="Default: undefined"
+              />
+              <Card
+                name="Dash style"
+                popoverItems={[
+                  "Solid",
+                  "ShortDash",
+                  "ShortDot",
+                  "ShortDashDot",
+                  "ShortDashDotDot",
+                  "Dot",
+                  "Dash",
+                  "LongDash",
+                  "DashDot",
+                  "LongDashDot",
+                  "LongDashDotDot"
+                ]}
+                link="https://api.highcharts.com/highcharts/plotOptions.line.dashStyle"
+                implementation="{plotOptions: {line: {dashStyle: value}}"
+                textFieldPlaceHolder="Default: solid"
+              />
+            </Stack>
+          </div>
         </Frame>
       </AppProvider>
     );
